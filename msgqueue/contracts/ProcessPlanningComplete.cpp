28f2158f014cb78d3f29d24108abeff3dbff2f78
unbusy <unbusy@lhost.com>
2021-09-10 01:55:56 +0200

    Implement toJson()

--- msgqueue/contracts/ProcessPlanningComplete.cpp
+++ msgqueue/contracts/ProcessPlanningComplete.cpp
@@ -8,8 +8,34 @@ std::string ProcessPlanningComplete::EventName()
 json ProcessPlanningComplete::toJson()
 {
     json jsonObject;
-    jsonObject["user_id"] = userID;
-    jsonObject["cadfile_id"] = cadFileID;
-    jsonObject["task_id"] = taskID;
-    return jsonObject;
+
+    std::vector<json> bendSequence;
+
+    for (auto &bendID : processingPlan.bendSequence)
+    {
+        json jsonBendID = {
+            {"bend_id", bendID},
+        };
+        bendSequence.push_back(jsonBendID);
+    }
+
+    json processingplan = {
+        {"cadfile_id", cadFileID},
+        {"rotations", processingPlan.rotations},
+        {"flips", processingPlan.flips},
+        {"tools", processingPlan.tools},
+        {"modules", processingPlan.modules},
+        {"processing_time", processingPlan.processingTime},
+        {"quantity", 1},
+        {"total_tool_distance", processingPlan.totalToolDistance},
+        {"bend_sequences", bendSequence}};
+
+    json resultJson;
+    resultJson["pp_plan"] = processingplan;
+    resultJson["process_level"] = processLevel;
+    resultJson["user_id"] = userID;
+    resultJson["cadfile_id"] = cadFileID;
+    resultJson["task_id"] = taskID;
+
+    return resultJson;
 }
